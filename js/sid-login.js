bf238f39ee7235ced4278cf9ef1990cba499d024
Sid S <sid@Sids-MacBook-Pro.local>
2019-02-26 12:13:59 -0600

    fixed login & links on db

--- js/sid-login.js
+++ js/sid-login.js
@@ -1,6 +1,8 @@
 var el = document.getElementById("login-btn")
 
 function checkDetails() {
+    
+    
     var username = document.getElementById("exampleInputEmail").value;
     var password = document.getElementById("exampleInputPassword").value;
     
@@ -9,33 +11,44 @@ function checkDetails() {
     
     var existing = window.localStorage.getItem('userlist');
         existing = existing ? existing.split(',') : [];
+    console.log(existing);
+    debugger;
     
+    var flag = false;
     if(existing.length==0){
+        console.log("I entered the if where existing.length = 0")
+        debugger;
         document.getElementById("error-message").style="color: red;";
         el.href="#";
         return false;
     }
     else{
-        var flag = false;
-        {
-            var arrayLength = existing.length;
-            for(var i = 0; i< arrayLength; i++){
-                if(existing[i].includes(user1)){
-                    flag = true;
-                    break;
-                }
+        console.log("I entered the else where existing existing earlier. Existing is: ")
+        console.log(existing);
+        debugger;
+        var arrayLength = existing.length;
+        for(var i = 0; i< arrayLength; i++){
+            if(((existing[i].includes(user1))==true) && user1!=""){
+                flag = true;
+                break;
             }
         }
         
         if(flag==false){
+            console.log("Flag is false.")
+            debugger;
             document.getElementById("error-message").style="color: red;";
             el.href="#";
             return false;
         }
         else{
+            console.log("Flag is true.")
+            debugger;
             window.localStorage.setItem("CurrentUser",user1);
             el.href="dashboard.html";
             return true;   
         }
     }
 }
+
+el.onclick = checkDetails;
