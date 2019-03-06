e40c4b468c416d15ae8377aa217d2fffc15cf736
Javier <javiercuadra2020@u.northwestern.edu>
2019-03-05 21:46:16 -0600

    Began work on creating project tabs

--- js/createProject.js
+++ js/createProject.js
@@ -1,10 +1,17 @@
 function makeNewTab(){
-	let projectName = document.getElementById("projectName")
-	let tabs = document.getElementById("nav-tab")
+	let tabs = document.getElementById("myTab")
+	let tabPanels = document.getElementById("tab-content")
 
-	tabs.innerHTML = projectName
+	let tabName = document.getElementById("projectName").value
+	let projectDeadline = document.getElementById("projectDeadline").value
+	let tabHTML = "<li class=\"nav-item\">\n<a class=\"nav-link active\" id=\"home-tab\" data-toggle=\"tab\" href=\"#" + tabName + "\" role=\"tab\" aria-controls=\"home\" aria-selected=\"true\">" + tabName + "</a>\n</li>"
+	let tabPaneHTML = "<div class=\"tab-pane fade\" id=\"" + tabName + "\" role=\"tabpanel\">" +"hello, I rule!" + "</div>"
+
+	$('nav nav-tabs').add(tabHTML)
+	$('tab-content').add(tabPaneHTML)
+
 }
 
 function createProject(){
-	alert("Hi")
+	makeNewTab()
 }
