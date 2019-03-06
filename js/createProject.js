98628e797d0e0709d9ec88ab23ba19115d61ab2b
Javier <javiercuadra2020@u.northwestern.edu>
2019-03-05 22:50:45 -0600

    Edit to original tabbing method

--- js/createProject.js
+++ js/createProject.js
@@ -1,14 +1,40 @@
+
+
 function makeNewTab(){
 	let tabs = document.getElementById("myTab")
-	let tabPanels = document.getElementById("tab-content")
+	let tabPanes = document.getElementById("myTabContent")
 
 	let tabName = document.getElementById("projectName").value
 	let projectDeadline = document.getElementById("projectDeadline").value
 	let tabHTML = "<li class=\"nav-item\">\n<a class=\"nav-link active\" id=\"home-tab\" data-toggle=\"tab\" href=\"#" + tabName + "\" role=\"tab\" aria-controls=\"home\" aria-selected=\"true\">" + tabName + "</a>\n</li>"
-	let tabPaneHTML = "<div class=\"tab-pane fade\" id=\"" + tabName + "\" role=\"tabpanel\">" +"hello, I rule!" + "</div>"
+	let tabPaneHTML = "<div class=\"tab-pane fade\" id=\"" + tabName + "\" role=\"tabpanel\">" + "hello, I rule!" + "</div>"
 
-	$('nav nav-tabs').add(tabHTML)
-	$('tab-content').add(tabPaneHTML)
+
+    let tab = document.createElement("li");
+    tab.setAttribute("class", "nav-item");
+
+    let link = document.createElement("a");
+    link.setAttribute("class", "nav-link active");
+    link.setAttribute("id", "tab" + tabName);
+    link.setAttribute("data-toggle", "tab");
+    link.setAttribute("href", "#" + tabName);
+    link.setAttribute("role", "tab");
+    link.setAttribute("aria-controls", "home");
+    link.setAttribute("aria-selected", "true");
+
+    link.innerHTML = tabName
+
+    tab.appendChild(link)
+    tabs.appendChild(tab)
+
+    let tabPane = document.createElement("div")
+    tabPane.setAttribute("class", "tab-pane fade")
+    tabPane.setAttribute("id", "panel" + tabName)
+    tabPane.setAttribute("role", "tabpanel")
+
+    tabPane.innerHTML = projectDeadline
+
+    tabPanes.appendChild(tabPane)
 
 }
 
