21860c320a357c298a9334117e59c2f3885046cb
Javier <javiercuadra2020@u.northwestern.edu>
2019-03-05 18:23:25 -0600

    Began tinkering with the modal in to create projects

--- js/addCollaborators.js
+++ js/addCollaborators.js
@@ -4,6 +4,21 @@ function addCollab(){
 
   let email = document.getElementById("email-form").value;
   document.getElementById("email-form").value = "";
-  collaborators.push(email);
-  document.getElementById("shared-with").innerHTML = "Shared With: " + collaborators;
+
+  if (validateEmail(email)) 
+  {
+  	collaborators.push(" " + email);
+  	document.getElementById("shared-with").innerHTML = "Shared With: " + collaborators + " ";
+  }
+  else
+  {
+  	window.alert("Please input a valid email.")
+  }
+
+}
+
+//Found this code online to help with email validations
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
---
+++ js/createProject.js
@@ -0,0 +1,7 @@
+function makeNewTab(){
+	
+}
+
+function createProject(){
+
+}
