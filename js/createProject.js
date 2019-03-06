3604b4d203eccdb6d768c41195156ab69e9fec98
Javier <javiercuadra2020@u.northwestern.edu>
2019-03-05 19:16:29 -0600

    Made addCollabs and addProjects have a date field and removed the reference to dashboard in projects

--- js/createProject.js
+++ js/createProject.js
@@ -1,7 +1,10 @@
 function makeNewTab(){
-	
+	let projectName = document.getElementById("projectName")
+	let tabs = document.getElementById("nav-tab")
+
+	tabs.innerHTML = projectName
 }
 
 function createProject(){
-
+	alert("Hi")
 }
